af1881e3884a01155d639202cf9a018afa34e90d
Agustina Mendez <agustinabmendez@gmail.com>
2022-05-31 20:29:38 -0300

    equals

--- tp-progra2/src/trabajo/Transporte.java
+++ tp-progra2/src/trabajo/Transporte.java
@@ -86,7 +86,7 @@ public abstract class Transporte {
 			return false;
 		}
 		Transporte t = (Transporte) obj;
-		return this.getClass().getSimpleName().equals(t.getClass().getSimpleName()) && this.destino.equals(t.destino)
+		return this.getClass().equals(t.getClass()) && this.destino.equals(t.destino)
 				&& this.cargaActual == t.cargaActual;
 	}
 }
